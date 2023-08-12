70c21353c7fd2b3bff9a9aa642626313fc431668
cunba <ire.cunba@gmail.com>
2023-08-12 02:33:39 +0200

    create new tab to download archives from txt file + improve code

--- src/main/java/com/svalero/multidescarga/controller/AppController.java
+++ src/main/java/com/svalero/multidescarga/controller/AppController.java
@@ -2,6 +2,7 @@ package com.svalero.multidescarga.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import com.svalero.multidescarga.util.R;
@@ -9,6 +10,8 @@ import com.svalero.multidescarga.util.R;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -20,10 +23,11 @@ public class AppController implements Initializable {
     @FXML
     Tab tabDownloads;
     @FXML
+    Tab tabDownloadsFromFile;
+    @FXML
     Tab tabRecord;
 
     Stage stage;
-    RecordController recordController;
 
     public AppController(Stage stage) {
         this.stage = stage;
@@ -31,34 +35,45 @@ public class AppController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Tab tabSelected = tabPane.getSelectionModel().getSelectedItem();
         FXMLLoader loaderDownloads = new FXMLLoader();
+        FXMLLoader loaderDownloadsFromFile = new FXMLLoader();
         FXMLLoader loaderRecord = new FXMLLoader();
 
-        recordController = new RecordController();
-        loaderDownloads.setLocation(R.getUI("downloads-view.fxml"));
-        loaderDownloads.setController(new DownloadsController(stage, recordController));
-
+        RecordController recordController = new RecordController();
         loaderRecord.setLocation(R.getUI("record-view.fxml"));
         loaderRecord.setController(recordController);
 
-        if (tabSelected == tabDownloads)
-            try {
-                ScrollPane downloads = loaderDownloads.load();
-                tabDownloads.setContent(downloads);
-                ScrollPane record = loaderRecord.load();
-                tabRecord.setContent(record);
-            } catch (IOException e) {
-                e.printStackTrace();
+        DownloadsController downloadsController = new DownloadsController(stage, recordController);
+        loaderDownloads.setLocation(R.getUI("downloads-view.fxml"));
+        loaderDownloads.setController(downloadsController);
+
+        DownloadsFromFileController downloadsFromFileController = new DownloadsFromFileController(stage,
+                recordController);
+        loaderDownloadsFromFile.setLocation(R.getUI("downloads_from_file-view.fxml"));
+        loaderDownloadsFromFile.setController(downloadsFromFileController);
+
+        try {
+            ScrollPane downloads = loaderDownloads.load();
+            tabDownloads.setContent(downloads);
+            ScrollPane downloadsFromFile = loaderDownloadsFromFile.load();
+            tabDownloadsFromFile.setContent(downloadsFromFile);
+            ScrollPane record = loaderRecord.load();
+            tabRecord.setContent(record);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+                "Al cerrar la aplicación se pararán todas las descargas en curso");
+        stage.setOnCloseRequest((t) -> {
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                downloadsController.onCloseRequest();
+                downloadsFromFileController.onCloseRequest();
+                stage.close();
+            } else {
+                t.consume();
             }
-        else
-            try {
-                ScrollPane record = loaderRecord.load();
-                tabRecord.setContent(record);
-                ScrollPane downloads = loaderDownloads.load();
-                tabDownloads.setContent(downloads);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        });
     }
 }
